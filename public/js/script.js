975410ddfc99926ab8fc64bce1f54780778d1322
gavin-love <1co.loco@oprotonmail.ch>
2018-08-14 14:32:09 -0600

    Add function to generate random colors

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,18 @@
 const generateColorPalette = e => {
   if (e.which === 32) {
-    console.log('wlkjasdlkfj')
+
+    const randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    for (let i = 0; i <= 4; i++) {
+      $('.palette_one').css("background-color", `${randomColor()}`)
+      $('.palette_two').css("background-color", `${randomColor()}`)
+      $('.palette_three').css("background-color", `${randomColor()}`)
+      $('.palette_four').css("background-color", `${randomColor()}`)
+      $('.palette_five').css("background-color", `${randomColor()}`)
+    }
   }
 }
 
 $(window).on('keypress', generateColorPalette);
+
+
