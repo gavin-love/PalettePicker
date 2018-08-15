38cab271eeb42459ddb971d0d835b4ed5c1e1226
gavin-love <1co.loco@oprotonmail.ch>
2018-08-15 15:38:56 -0600

    Write post functions on server for projects and palettes

--- server.js
+++ server.js
@@ -14,6 +14,41 @@ app.get('/', (request, response) => {
 
 });
 
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+
+  for (let requiredParameter of 'title') {
+    if (!project[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: {title: <string>}. You're missing a "${requiredParameter}" property.`
+      });
+    }
+
+    database('projects').insert(project, 'id')
+      .then(project => response.status(200).json({ id: project[0] }))
+      .catch(error => response.status(500).json({ error }))
+  }
+})
+
+app.post('/api/v1/palette', (request, response) => {
+  const palette = request.body;
+
+  for (let requiredParameter of 'title') {
+    if (!palette[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: {title: <string>}. You're missing a "${requiredParameter}" property.`
+      });
+    }
+
+    database('palette').insert(palette, 'id')
+      .then(palette => response.status(200).json({ id: palette[0] }))
+      .catch(error => response.status(500).json({ error }))
+  }
+})
+
+
+
+
 app.get('/json', (request, response) => {
   response.status(200).json();
 });
