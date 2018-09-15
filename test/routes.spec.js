71cc7d11aec7beb18fc60b27d54550d52861a9ec
gavin-love <1co.loco@oprotonmail.ch>
2018-09-15 11:20:20 -0600

    Add test for POST projecst to db
    Add test for GET projecst from db

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -114,3 +114,87 @@ describe('/api/v1/palettes', () => {
       });
   });
 });
+
+
+
+
+
+describe('/api/v1/projects', () => {
+  beforeEach(done => {
+    database.migrate.rollback()
+      .then(() => database.migrate.latest())
+      .then(() => database.seed.run())
+      .then(() => done());
+  });
+
+  it('should return a list of projects when a GET request is made', (done) => {
+    chai.request(server)
+      .get('/api/v1/projects')
+      .end((error, response) => {
+        should.equal(error, null);
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.should.have.length(1);
+        response.body[0].should.be.a('object');
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('title');
+        response.body[0].id.should.be.a('number');
+        response.body[0].title.should.be.a('string');
+        response.body[0].id.should.equal(1);
+        response.body[0].title.should.equal('John/s APP');
+        done();
+      })
+  })
+
+  it('should return a specific project when a GET request is made', (done) => {
+    chai.request(server)
+      .get('/api/v1/projects/John/s APP')
+      .end((error, response) => {
+        should.equal(error, null);
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.should.have.length(1);
+        response.body[0].should.be.a('object');
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('title');
+        response.body[0].id.should.be.a('number');
+        response.body[0].title.should.be.a('string');
+        response.body[0].id.should.equal(1);
+        response.body[0].title.should.equal('John/s APP');
+        done();
+      })
+  })
+
+  it('should insert a project when a POST request is made', (done) => {
+    chai.request(server)
+      .post('/api/v1/projects')
+      .send({
+        title: 'John/s APP'
+      })
+      .end((error, response) => {
+        should.equal(error, null);
+        response.should.have.status(201);
+        response.should.be.json;
+        response.body.should.be.a('object');
+        response.body.should.have.property('id');
+        response.body.id.should.be.a('number');
+        response.body.id.should.equal(2);
+        done();
+      });
+  });
+
+  it.only('throws an error if request params are missing when a POST request is made', (done) => {
+    chai.request(server)
+      .post('/api/v1/palettes')
+      .send({
+      })
+      .end((error, response) => {
+        response.should.have.status(422);
+        response.should.be.json;
+        response.body.should.be.a('object');
+        done();
+      });
+  });
+});
